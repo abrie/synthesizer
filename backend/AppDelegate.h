6618e3547aa34be26fb3751fbbdce8b3101a275d
abrie <abrie@errante.local>
2012-07-21 11:40:49 -0400

    Initial Commit

---
+++ backend/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  backend
+//
+//  Created by abrie on 2012-07-21.
+//  Copyright (c) 2012 abrie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ backendTests/backendTests.h
@@ -0,0 +1,13 @@
+//
+//  backendTests.h
+//  backendTests
+//
+//  Created by abrie on 2012-07-21.
+//  Copyright (c) 2012 abrie. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface backendTests : SenTestCase
+
+@end
